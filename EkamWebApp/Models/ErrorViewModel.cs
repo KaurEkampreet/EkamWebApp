1ac18e24465f878971d49a192240839047095f95
Ekampreet Kaur <Kaur.ekampreet@outlook.com>
2021-09-14 14:59:38 -0700

    Add project files.

---
+++ EkamWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EkamWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
